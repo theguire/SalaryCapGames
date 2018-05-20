43f1429015c74e3e2e66153634a1b8fb9c20bcec
Stephen McGuire <stephen.g.mcguire@gmail.com>
2018-05-20 15:34:40 -0700

    simple changes

---
+++ SalaryCapGames/wwwroot/js/Charts.js
@@ -0,0 +1,66 @@
+﻿
+var ctx6 = document.getElementById("myChart6");
+var myChart6 = new Chart(ctx6, {
+    type: 'bar',
+    data: {
+        labels: ["6/24", "6/25", "6/26", "6/27", "6/28", "6/29", "6/30"],
+        datasets: [{
+            data: [25, 10, 30, 21, 17, 30, 14],
+            lineTension: 0,
+            backgroundColor: 'transparent',
+            borderColor: '#007bff',
+            borderWidth: 4,
+            pointBackgroundColor: '#007bff',
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        legend: {
+            display: false,
+        }
+    }
+});
+
+var ctx5 = document.getElementById("myChart5");
+var myChart5 = new Chart(ctx5, {
+    type: 'bar',
+    data: {
+        labels: ["6/24", "6/25", "6/26", "6/27", "6/28", "6/29", "6/30"],
+        datasets: [{
+            data: [25, 10, 30, 21, 17, 30, 14],
+            lineTension: 0,
+            backgroundColor: 'transparent',
+            borderColor: '#007bff',
+            borderWidth: 4,
+            pointBackgroundColor: '#007bff',
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        legend: {
+            display: false,
+        }
+    }
+});
---
+++ SalaryCapGames/wwwroot/js/dashboard.index.js
@@ -0,0 +1,25 @@
+﻿google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.setOnLoadCallback(generateChart); 
+
+function generateChart() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Products');
+    data.addColumn('number', 'Sales');
+    data.addRows([
+        ['Tofu', 30],
+        ['Chai', 10],
+        ['Chocolade', 20],
+        ['Ipoh Coffee', 40]
+    ]);
+
+    var options = {
+        'title': 'Quarterly Sales',
+        'width': 600,
+        'height': 300,
+        is3D: true,
+        colors: ['#d9534f', '#f0ad4e', '#5bc0de', '#5cb85c']
+    };
+
+    var chart = new google.visualization.PieChart($('#piechart')[0]);
+    chart.draw(data, options);
+} 
